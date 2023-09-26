4bcd6c2aede524fbfb886189f2488c3b36149038
Solomon Adewole <solomonwolemail@gmail.com>
2023-09-26 11:09:33 +0100

    mend

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -6,7 +6,7 @@ const appContext = createContext();
 export function AppContextProvider({ children }) {
   const [appData, setAppData] = useState(dataApp);
   // Set your target launch date and time
-  const targetDate = new Date('2023-09-28T00:00:00Z').getTime();
+  const targetDate = new Date('2023-09-26T00:00:00Z').getTime();
   // Initialize state variables
   const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining());
 
