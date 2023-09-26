19bb638048cef5ecd15e7c41e75bf9b619b822cf
Solomon Adewole <solomonwolemail@gmail.com>
2023-09-26 11:28:23 +0100

    mend

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -24,18 +24,18 @@ export function AppContextProvider({ children }) {
       };
     }
 
-    const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-      (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+    // const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    // const hours = Math.floor(
+    //   (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    // );
+    // const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+    // const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
 
     return {
-      days,
-      hours,
-      minutes,
-      seconds,
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
     };
   }
 
